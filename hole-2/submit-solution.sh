671c3df55af86140ed70adf3863d04dc417e8703
Robert Taylor <robert.taylor426@gmail.com>
2017-07-21 12:02:03 +0100

    Adding submit solution scripts.

---
+++ hole-2/submit-solution.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -e
+
+function printUsage {
+    echo "Usage: ./submit_solution.sh --author [AUTHOR] --solution [SOLUTION]"
+}
+
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            printUsage
+            exit 0
+            ;;
+        -a|--author)
+            shift
+            if test $# -gt 0; then
+                export AUTHOR=$1
+            else
+                echo "no author specified"
+                exit 1
+            fi
+            shift
+            ;;
+        -s|--solution)
+            shift
+            if test $# -gt 0; then
+                export SOLUTION=$1
+            else
+                echo "no solution specified"
+                exit 1
+            fi
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if [[ -z $AUTHOR ]] || [[ -z $SOLUTION ]] ; then
+    printUsage
+    exit 1
+fi
+
+curl -X POST -H "Content-Type: application/json" http://35.176.237.81:3000/exercises/vice_versa/authors/$AUTHOR/solutions -d '{"solution": "$SOLUTION"}'
